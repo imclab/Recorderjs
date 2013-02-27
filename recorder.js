d3cf8225c26f1ece9af8282e61c58615e4540499
Norman Xu <homyu.shinn@gmail.com>
2013-02-27 11:09:30 +0800

    compatibile with new api

--- recorder.js
+++ recorder.js
@@ -12,7 +12,8 @@ define(['module'], function(module){
     var config = cfg || {isMono: false};
     var bufferLen = config.bufferLen || 4096;
     this.context = source.context;
-    this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+    this.node = (this.context.createScriptProcessor || this.context.createJavaScriptNode)
+      .call(this.context, bufferLen, 2, 2);
     var worker = new Worker(config.workerPath || WORKER_PATH);
     worker.postMessage({
       command: 'init',
